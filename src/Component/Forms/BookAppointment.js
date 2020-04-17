5b2be81f4f15cf619943290fb48eb482184a2e3b
sabbasachiPH <rentustat02@gmail.com>
2020-04-17 23:30:40 +0600

    add button problem fix

--- src/Component/Forms/BookAppointment.js
+++ src/Component/Forms/BookAppointment.js
@@ -45,7 +45,7 @@ const BookAppointment = (props) => {
     console.log(time, patientName, phoneNumber, emailAddress, appointmentDate);
     console.log(appointmentDetail);
     // fetch("http://localhost:4200/addAppointment", {
-    fetch("http://red-mongo.herokuapp.com/addAppointment", {
+    fetch("https://red-mongo.herokuapp.com/addAppointment", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
